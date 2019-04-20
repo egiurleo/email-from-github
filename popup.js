b4cab45fdba1e1d5d29bc8a88d2be5c35677825f
Emily Giurleo <e.m.giurleo@gmail.com>
2019-04-20 12:16:05 -0400

    cleaned up error state

--- popup.js
+++ popup.js
@@ -1,17 +1,3 @@
-// #!/bin/bash
-//
-// handle=$1
-// target="https://api.github.com/users/TEMP/events/public"
-// target=${target//TEMP/$handle}
-// echo "Target URL: $target"
-// tmp=$(curl -A "Mozilla/5.0" -L -k -b /tmp/c -c /tmp/c -s $target | egrep -o "\b[a-zA-Z0-9.-]+@[a-zA-Z0-9.-]+\.[a-zA-Z0-9.-]+\b" | sort | uniq)
-// if [ "$tmp" = "" ]; then
-//     echo "No Email Addresses Found"
-// else
-// 	echo "Email Addresses:"
-// 	echo "$tmp"
-// fi
-
 function getEventInfo(userId, callback) {
   var xhr = new XMLHttpRequest();
   xhr.open('GET', `https://api.github.com/users/${userId}/events/public`, true);
@@ -23,15 +9,20 @@ function getEventInfo(userId, callback) {
       var response = JSON.parse(xhr.responseText);
       response.forEach(event => {
         if(event.payload && event.payload.commits) {
+          var emailFound = false;
+
           event.payload.commits.forEach(commit => {
             if(commit.author && commit.author.email) {
               const email = commit.author.email;
               if(email) {
-                callback(email);
+                callback({ error: false, email });
+                emailFound = true;
                 return;
               }
             }
           });
+
+          if(!emailFound) { callback({ error: true }); }
         }
       });
     }
@@ -48,9 +39,17 @@ document.addEventListener('DOMContentLoaded', () => {
     const tabUrl = tabs[0].url;
     const githubId = tabUrl.split('/')[3];
 
-    getEventInfo(githubId, email => {
+    getEventInfo(githubId, response => {
       var div = document.getElementById('email');
-      div.innerHTML = `Email: ${email}`;
+      let html;
+
+      if(response.error) {
+        html = 'No email found.'
+      } else {
+        html = `Email: ${response.email}`;
+      }
+
+      div.innerHTML = html;
     });
   });
 });
